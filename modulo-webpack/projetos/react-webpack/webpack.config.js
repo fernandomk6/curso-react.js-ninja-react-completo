34688b999d5ab099ad116b3e0b7c0737035fcec6
Fernando Henrique <fernandomk6@gmail.com>
2022-12-14 09:09:53 -0300

    Update webpack.config.js

--- modulo-webpack/projetos/react-webpack/webpack.config.js
+++ modulo-webpack/projetos/react-webpack/webpack.config.js
@@ -4,11 +4,13 @@ const path = require('path')
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index'),
+  
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
+  
   module: {
     loaders: [{
       test: /\.js$/,
