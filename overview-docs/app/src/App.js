18e22b8b0202d5da4fe9b008b25ed3b15aad4f0b
fernando <fernandomk6@gmail.com>
2022-12-27 17:54:40 -0300

    Corrigido estrutura básica app react de testes

--- overview-docs/app/src/App.js
+++ overview-docs/app/src/App.js
@@ -1,48 +1,11 @@
-import './App.css';
+import './App.css'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>{count}</div>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <h1>App</h1>
     </div>
   )
 } 
-
-let firstRender = false // forma que ferificaremos se é o primeiro render
-let internalState // aonde será armazenada o valor do state
-
-// função que iria alterar o internalState
-function internalSetState (newState) {
-  console.log('internal set state')
-  internalState = newState // alterando o valor do state para o valor parassado para setState
-  window.render() // simulando a renderização novamente do componente
-}
-
-function useState (initialState) {
-  // O react tem como saber quando um componente renderiza a primeira vez
-  // e fazer um comportamente diferente na primeira e na segunda vez que o componente
-  // é renderizado
-
-  console.log('use state')
-
-  // Se não for o primeiro render, o initialValue será ignorado, e será usado
-  // o valor do estado atual existente
-  if (!firstRender) {
-    firstRender = true
-    internalState = initialState // seta o estado inicial apenas se for a primeira renderização
-  }
-
-  console.log({internalState})
-  // retorna o internalState que armazena o valor do estado
-  // é uma variavel de escopo léxico
-  // internal set state é passada também para poder alterar o state dentro do componente
-  // quando internal set state for executada, o valor do state muda e é feito uma rerenderização
-  // porem o react sabera que não deve setar o valor inicial do state pois ele já foi setado
-  return [internalState, internalSetState] 
-}
 
 export default App
--- overview-docs/app/src/index.js
+++ overview-docs/app/src/index.js
@@ -5,26 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function render () {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
-
-render()
-
-window.render = render 
-// compartilhando forma de renderizar a tela
-// para simular o que o react faz
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
